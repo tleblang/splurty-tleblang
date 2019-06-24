d8fd67982b648bc3f771bdbdd22d65d0c44e3bf1
T LeBlang <thanya2029@gmail.com>
2019-06-24 07:46:00 +0000

    validate user data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,7 +10,10 @@ class QuotesController < ApplicationController
   end
 
   def create
-  	Quote.create(quote_params)
+  	@quote = Quote.create(quote_params)
+
+  	if @quote.invalid?
+  		flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   	redirect_to root_path
   end
 
