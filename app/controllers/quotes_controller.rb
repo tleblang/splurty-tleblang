55c530c63044022c6c59bf33594612a3d4dd913f
T LeBlang <thanya2029@gmail.com>
2019-06-25 05:18:46 +0000

    adding a modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,10 @@
 class QuotesController < ApplicationController
-	 def index
-	 	
+	 
+   def index
 	 	@quote = Quote.order("RANDOM()").first
   end
 
-  def new
-  	@quote = Quote.new
 
-  end
 
   def create
   	@quote = Quote.create(quote_params)
